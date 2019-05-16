fd62131071225e6c5c49aac033fd61589875cc3d
Ricardo C Lima <ricardo.carvalho.lima1@hotmail.com>
2019-05-16 08:23:01 -0300

    Create comandos_turtle.py

---
+++ comandos_turtle.py
@@ -0,0 +1,11 @@
+import turtle               # chamar o turtle
+tartaruga = turtle.Turtle() # definir o turtle
+tartaruga.shape('turtle')   # definir o formato do turtle
+tartaruga.color('purple')   # definir a cor do turtle
+tartaruga.forward()         # movimentar para a frente
+tartaruga.backward()        # movimentar para tras
+tartaruga.left()            # movimentar para a esquerda
+tartaruga.right()           # movimentar para a direita
+tartaruga.speed()           # velocidade
+tartaruga.up()              # 'levanta' a caneta
+tartaruga.down()            # 'abaixa' a  caneta

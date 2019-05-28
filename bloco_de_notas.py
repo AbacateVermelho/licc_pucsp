9884d5d29349fa6ece3dee10fdfade825e19f509
Ricardo C Lima <ricardo.carvalho.lima1@hotmail.com>
2019-05-28 12:11:58 -0300

    Create bloco_de_notas.py
    Comando para ultilizar o bloco de notos com o python.

---
+++ bloco_de_notas.py
@@ -0,0 +1,10 @@
+arquivo = open('b.txt', 'w')
+
+arquivo.write('Conteudo dento do arquivo')
+
+arquivo.close()
+
+#r = leitura
+#r+ = leitura e escrita
+#a = acrescenta sem mexer no que já tinha no bloco
+#w = escrita, substituindo o que tinha no bloco
